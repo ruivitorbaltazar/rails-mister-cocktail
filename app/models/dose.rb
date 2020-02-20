6f280179f341a846778c007a3c3f9272739437f7
Rui Baltazar <ruivrb@gmail.com>
2020-02-20 12:47:27 +0000

    rails new with proper postgres

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :description, presence: true
+  validates :description, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+## Ingredient Seed
+puts 'Throwing out all the ingredients 🗑'
+Ingredient.delete_all
+
+puts 'Buying 3 ingredients 🛒'
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'mint leaves')
+
+puts 'Ingredients are set! 🍶'
+
+## Cocktail Seed
+# puts 'Pouring all cocktails down the drain 😩'
+# Cocktail.delete_all
+
+# puts 'Making 3 cocktails 🍹'
+# Cocktail.create(name: 'Whiskey Sour')
+# Cocktail.create(name: 'Margarita')
+# Cocktail.create(name: 'Negroni')
+
+# puts 'Drinks are served! 🍻'
